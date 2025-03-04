0d6286f35cb36d102ea5d5fef4d2f9ac3eca351c
AyoubElmahjoubi <168941231+AyoubElmahjoubi@users.noreply.github.com>
2025-03-03 23:52:03 -0800

    initial commit

---
+++ app/src/main/java/com/example/tp_app/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tp_app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.idtxt);
+        text.setText("hello TDI !");
+
+    }
+}
